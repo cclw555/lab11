2b3443c7e6ca6ba7c0d98738419caa115ec08a48
Zongrui Liu <royliu1995@gmail.com>
2023-11-29 10:01:25 -0500

    test error 3

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def has_letter_cases(s: str):
     """
     return not s.isupper() and not s.islower()
 
-def find_lowercase_vwel(msg: str):
+def find_lowercase_vwel(msg: st):
     """
     Return the index of the first lowercase vowel (a, e, i, o, u) in msg, 
     or the length of msg if it does not contain any lowercase vowels.
